b6269425d85ae31c9a672ac9682cc8b7fdaa8667
minjong kim <alswhddkdl@gmail.com>
2023-02-04 17:15:46 +0900

    FEAT: 로그인 상태 유지를 할 preference 세팅

--- Furry_Friends_android/app/src/main/java/com/k_bootcamp/Application.kt
+++ Furry_Friends_android/app/src/main/java/com/k_bootcamp/Application.kt
@@ -1,15 +1,16 @@
 package com.k_bootcamp
 
 import android.app.Application
+import com.k_bootcamp.furry_friends.util.preference.PreferenceUtils
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class Application: Application() {
-//    companion object {
-//        lateinit var prefs: PreferenceUtils
-//    }
+    companion object {
+        lateinit var prefs: PreferenceUtils
+    }
     override fun onCreate() {
-//        prefs = PreferenceUtils(applicationContext)
+        prefs = PreferenceUtils(applicationContext)
         super.onCreate()
     }
 }
---
+++ Furry_Friends_android/app/src/main/java/com/k_bootcamp/furry_friends/util/preference/PreferenceUtils.kt
@@ -0,0 +1,34 @@
+package com.k_bootcamp.furry_friends.util.preference
+
+import android.app.Activity
+import android.content.Context
+
+class PreferenceUtils(context: Context) {
+    private val prefName = "prefs"
+    private val prefs = context.getSharedPreferences(prefName, Activity.MODE_PRIVATE)
+
+    var session:String?
+        get() = prefs.getString("session", null)
+        set(value){
+            prefs.edit().putString("session", value).apply()
+        }
+    var userId:String?
+        get() = prefs.getString("userId", null)
+        set(value) {
+            prefs.edit().putString("userId", value).apply()
+        }
+    var email:String?
+        get() = prefs.getString("email", null)
+        set(value) {
+            prefs.edit().putString("email", value).apply()
+        }
+    var name:String?
+        get() = prefs.getString("name", null)
+        set(value) {
+            prefs.edit().putString("name", value).apply()
+        }
+
+    fun clear() {
+        prefs.edit().clear().apply()
+    }
+}
