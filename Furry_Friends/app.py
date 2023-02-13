0bbe2e725a3d6e3bcaaf246a389b7619c69f3fc4
cosmosaur <119040660+c0smosaur@users.noreply.github.com>
2023-02-13 14:16:08 +0900

    FEAT: 관리 동물 변경 기능

--- Furry_Friends/app.py
+++ Furry_Friends/app.py
@@ -1,16 +1,19 @@
-from flask import Flask, g, session, request, jsonify, render_template
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import and_
+from flask import Flask, redirect, url_for, session
+from connect_db import db
 from flask_migrate import Migrate
 
-# for checklist
+
+# models
+from models import Animal
+
+
+# blueprints
 import authentification
 import routine
 import checklist
 import journal
 import health
 import pet
-from connect_db import db
 
 
 app = Flask(__name__)
@@ -30,6 +33,28 @@ db.init_app(app)
 
 
 Migrate(app,db)
+
+
+# 메인 화면
+@app.route('/', methods=["GET"])
+def main():
+
+    # 세션에 로그인한 기록이 있음
+    if 'login' in session: 
+        # 관리할 동물 선택 o --> 동물 프로필로 이동
+        if 'curr_animal' in session:
+            return redirect(url_for('pet.profile'))
+
+        # 관리할 동물 선택 x --> 동물 관리로 이동
+        else:
+            return redirect(url_for('pet.management'))
+
+        # return f"{session['user_id']} is logged in"
+
+    # 세션에 로그인한 기록이 없음
+    else:
+        # 로그인 페이지 리다이렉트
+        return redirect(url_for('authentification.login'))
 
 
 if __name__ == "__main__":
