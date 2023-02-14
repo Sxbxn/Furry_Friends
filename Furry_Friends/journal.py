78f99b44fe14e69764409732eae996c794d1e41a
cosmosaur <119040660+c0smosaur@users.noreply.github.com>
2023-02-14 11:37:27 +0900

    FIX: file_upload 함수 upload_fileobj로 교체, 파일 확장자 확인

--- Furry_Friends/journal.py
+++ Furry_Friends/journal.py
@@ -10,7 +10,7 @@ import os
 from werkzeug.utils import secure_filename
 # from predict import padding, mk_img, predict_result
 
-from config import AWS_ACCESS_KEY, AWS_SECRET_ACCESS_KEY, AWS_S3_BUCKET_NAME, AWS_S3_BUCKET_REGION
+from config import AWS_ACCESS_KEY, AWS_SECRET_ACCESS_KEY, AWS_S3_BUCKET_NAME, AWS_S3_BUCKET_REGION, ALLOWED_EXTENSIONS
 
 # app = Flask(__name__)
 bp = Blueprint('journal', __name__, url_prefix='/journal')
@@ -34,18 +34,29 @@ def s3_connection():
 
 def query_to_dict(objs):
     try:
-        lst = []
-        for obj in objs:
-            obj = obj.__dict__
+        lst = [obj.__dict__ for obj in objs]
+        for obj in lst:
             del obj['_sa_instance_state']
-            lst.append(obj)
         return lst
     except TypeError: # non-iterable
-        lst = []
+        if objs == None:
+            return []
         objs = objs.__dict__
         del objs['_sa_instance_state']
-        lst.append(objs)
+        lst = [objs]
         return lst
+
+
+def upload_file_to_s3(file):
+    filename = secure_filename(file.filename)
+    s3.upload_fileobj(
+            file,
+            AWS_S3_BUCKET_NAME,
+            file.filename
+            
+    )
+
+    return file.filename
 
 
 s3 = s3_connection()
@@ -54,12 +65,8 @@ s3 = s3_connection()
 # 기록 목록 출력
 @bp.route('/journals', methods = ['GET'])
 def journals():
-    # 유저아이디, 동물아이디
-    session['login'] = request.headers['user_id']
-    session['curr_animal'] = request.headers['animal_id']
 
-## ----------------------------------------------------------
-        # order by도 넣어야 할 듯(최신순?)
+    # order by도?
     journals = Journal.query.filter(and_(Journal.user_id==session['login'],
                                         Journal.animal_id==session['curr_animal'])).all()
     
@@ -76,10 +83,6 @@ def journals():
 # 아이템 클릭 시 기록 열람
 @bp.route('/content', methods=["GET"])
 def journal_content():
-    # 유저아이디, 동물아이디
-    session['login'] = request.headers['user_id']
-    session['curr_animal'] = request.headers['animal_id']
-
     journal_index = request.headers['index']
 
     journal_entry = Journal.query.get(int(journal_index))
@@ -93,9 +96,6 @@ def journal_content():
 # 기록 생성
 @bp.route('/factory', methods=["GET","POST"])
 def journal_factory():
-    # 유저아이디, 동물아이디, 날짜
-    session['login'] = request.headers['user_id']
-    session['curr_animal'] = request.headers['animal_id']
     currdate = request.headers['currdate']
 
     if request.method == "GET":
@@ -117,16 +117,22 @@ def journal_factory():
         
         # 사진 업로드 시 사진 링크 반환, 일상 기록 db 저장
         if f:
-            newname = (str(datetime.datetime.now()).replace(":","")).replace(" ","_") + ".png"
+            extension = f.filename.split('.')[-1]
+            if extension in ALLOWED_EXTENSIONS:
+                extension = '.' + extension
 
-            imgpath = f"./static/{secure_filename(newname)}"
-            f.save(imgpath) # 로컬에 저장
+                newname = (str(datetime.datetime.now()).replace(":","")).replace(" ","_") + extension
+                img_url = f"https://{AWS_S3_BUCKET_NAME}.s3.{AWS_S3_BUCKET_REGION}.amazonaws.com/{newname}"
+                f.filename = newname
 
-            s3.upload_file(imgpath, AWS_S3_BUCKET_NAME, newname) # s3에 업로드
-            img_url = f"https://{AWS_S3_BUCKET_NAME}.s3.{AWS_S3_BUCKET_REGION}.amazonaws.com/{newname}"
-            os.remove(imgpath) # 로컬에 저장된 파일 삭제
+                output = upload_file_to_s3(f)
+    
+                image = img_url
 
-            image = img_url
+            # 업로드된 파일이 이미지 파일이 아님
+            else:
+                return "wrong file extension"
+            
         # 사진 업로드 x시 image 링크
         else: 
             image = ""
@@ -142,10 +148,6 @@ def journal_factory():
 # 기록 수정
 @bp.route('/update', methods=["GET","PUT"])
 def journal_update():
-
-    # 유저아이디, 동물아이디
-    session['login'] = request.headers['user_id']
-    session['curr_animal'] = request.headers['animal_id']
     
     journal_index = request.headers['index']
     
@@ -182,20 +184,25 @@ def journal_update():
             # 기존에 이미지가 없었던 경우 -- pass
             except: 
                 pass
-
-            newname = (str(datetime.datetime.now()).replace(":","")).replace(" ","_") + ".png"
-            imgpath = f"./static/{secure_filename(newname)}"
-            f.save(imgpath)
             
-            s3.upload_file(imgpath, AWS_S3_BUCKET_NAME, newname)
-            img_url = f"https://{AWS_S3_BUCKET_NAME}.s3.{AWS_S3_BUCKET_REGION}.amazonaws.com/{newname}"
-            editing_entry.image = img_url
+            extension = f.filename.split('.')[-1]
+            if extension in ALLOWED_EXTENSIONS:
+                extension = '.' + extension
+
+                newname = (str(datetime.datetime.now()).replace(":","")).replace(" ","_") + extension
+                img_url = f"https://{AWS_S3_BUCKET_NAME}.s3.{AWS_S3_BUCKET_REGION}.amazonaws.com/{newname}"
+                f.filename = newname
+
+                output = upload_file_to_s3(f)
+
+                editing_entry.image = img_url
+            
+            else:
+                return "wrong file extension"
 
         # 새로 이미지 업로드 X --> 기존의 image 칼럼 데이터 그대로 유지
         else:
-            
             # 이미지가 있었는데 삭제하는 건?????
-
             pass
 
         editing_entry.title = title
