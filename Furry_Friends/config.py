8bdee05cb27b6b03f89b0daab4dfba5be28404a5
cosmosaur <119040660+c0smosaur@users.noreply.github.com>
2023-02-14 11:38:05 +0900

    FIX: 허용된 이미지 파일 확장자 리스트 추가

--- Furry_Friends/config.py
+++ Furry_Friends/config.py
@@ -1,6 +1,8 @@
 SQLALCHEMY_DATABASE_URI = 'sqlite:///pet_Test.db'
 SECRET_KEY = "test"
 
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+
 # AWS_ACCESS_KEY = 
 # AWS_SECRET_ACCESS_KEY = 
 AWS_S3_BUCKET_REGION = "ap-northeast-1"
