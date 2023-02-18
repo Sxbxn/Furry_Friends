5fbe63fc02658bdce2ced8d006d91e0aec43ca35
cosmosaur <119040660+c0smosaur@users.noreply.github.com>
2023-02-18 13:44:37 +0900

    FIX: 앱-서버 연결 수정(3)

--- Furry_Friends/config.py
+++
@@ -1,9 +0,0 @@
-SQLALCHEMY_DATABASE_URI = 'sqlite:///pet_Test.db'
-SECRET_KEY = "test"
-
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
-
-# AWS_ACCESS_KEY = 
-# AWS_SECRET_ACCESS_KEY = 
-AWS_S3_BUCKET_REGION = "ap-northeast-1"
-AWS_S3_BUCKET_NAME = "cosmosaurtest"
