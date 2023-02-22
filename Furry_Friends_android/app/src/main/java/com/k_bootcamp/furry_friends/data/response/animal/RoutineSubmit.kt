f19d6757441f0c056026c81a45fd306b1e2be97c
minjong kim <alswhddkdl@gmail.com>
2023-02-23 01:02:30 +0900

    CHORE: 사용하지 않는 클래스 제거

--- Furry_Friends_android/app/src/main/java/com/k_bootcamp/furry_friends/data/response/animal/RoutineSubmit.kt
+++
@@ -1,5 +0,0 @@
-package com.k_bootcamp.furry_friends.data.response.animal
-
-data class RoutineSubmit(
-    val isOk: Boolean
-)
--- Furry_Friends_android/app/src/main/java/com/k_bootcamp/furry_friends/data/response/user/SignInResponse.kt
+++
@@ -1,5 +0,0 @@
-package com.k_bootcamp.furry_friends.data.response.user
-
-data class SignInResponse(
-    val isOk: Boolean
-)
